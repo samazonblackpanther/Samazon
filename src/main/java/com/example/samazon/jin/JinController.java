1a15ffb7e722bf2eaa88343cc553100304b67167
jisookwon <jinskwon@hotmail.com>
2019-06-04 14:56:08 -0400

    nothing change

--- src/main/java/com/example/samazon/jin/JinController.java
+++ src/main/java/com/example/samazon/jin/JinController.java
@@ -1,9 +1,10 @@
 package com.example.samazon.jin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class JinController {
-
+@Autowired
 
 }
