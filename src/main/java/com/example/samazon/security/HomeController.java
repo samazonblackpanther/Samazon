ecc2667c02db8cdbb12bacc9ccfeb81e491491a4
Jacob Lefler <jcb.lefler@gmail.com>
2019-06-10 12:04:49 -0400

    d

--- src/main/java/com/example/samazon/security/HomeController.java
+++ src/main/java/com/example/samazon/security/HomeController.java
@@ -127,37 +127,24 @@ public class HomeController {
 
         User user = new User("samazonpanther@gmail.com",passwordEncoder.encode("password"),"Bob","Bobberson",true,"bob", "432 bob St", "Bobington", "Maine", "246342");
         user.setRoles(Arrays.asList(userRole));
-//        Address address = new Address("432 bob St", "Bobington", "Maine", "246342");
-//        addressRepository.save(address);
-//        user.setAddress(address);
+
         userRepository.save(user);
 
         user = new User("admin@adm.com",passwordEncoder.encode("password"),"Admin","User",true,"admin", "1234 Amind St", "Aton", "Maryland", "635896");
         user.setRoles(Arrays.asList(adminRole));
-//        address = new Address("1234 Amind St", "Aton", "Maryland", "635896");
-        addressRepository.save(address);
-//        user.setAddress(address);
+
         userRepository.save(user);
 
         //Sam
         user = new User("sam@smail.com", passwordEncoder.encode("password"), "Sam", "Sammy", true, "sam", "1234 Sammas St", "Samington", "Washington", "243342");
         user.setRoles(Arrays.asList(adminRole));
-//        address = new Address("1234 Sammas St", "Samington", "Washington", "243342");
-//        addressRepository.save(address);
+
 
         Product product = new Product("Vegetables Unleashed: A Cookbook by Jose Andres", "Books", 29.58, "Vegetables Unleashed is a new cookbook that will transform how we think about—and eat—the vast universe of vegetables. ", "https://res.cloudinary.com/db9bfssj4/image/upload/v1559761161/book.vegetables_zoy3ew.jpg"  );
         productRepository.save(product);
 
-//        Cart cart = new Cart(user, product);
-//        cartRepository.save(cart);
 
-//        History history = new History();
-//        history.setUser(user);
-//        history.setProducts(product);
 
-//        user.setAddress(address);
-//        user.setCarts(cart);
-//        user.setHistory(history);
         userRepository.save(user);
 
 
